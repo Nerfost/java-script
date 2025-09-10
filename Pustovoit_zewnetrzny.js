dad4da42998344d9e8d949f9efcee7d9a4aefdb0
Nerfost <nerfost2@gmail.com>
2025-09-10 19:15:02 +0200

    Add files via upload

--- Pustovoit_zewnetrzny.js
+++ Pustovoit_zewnetrzny.js
@@ -1,4 +1,4 @@
-document.write("ostatnia modyfikacja strony".fontcolor("#FF0000").bold().fontsize(5)+"<br>"+"<strike>"); 
+document.write("ostatnia modyfikacja strony".fontcolor("#FF0000").bold().fontsize(6)+"<br>"+"<strike>"); 
 
-document.write(document.lastModified);
+document.write(document.lastModified.fontcolor("#800000").bold().fontsize(7)+"<br>"+"</strike>");
 
