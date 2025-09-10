28ba67ba3d37873b536881e31dd045d4c3a6deb2
Nerfost <nerfost2@gmail.com>
2025-09-10 17:29:03 +0200

    Update Pustovoit_zewnetrzny.js

--- Pustovoit_zewnetrzny.js
+++ Pustovoit_zewnetrzny.js
@@ -1,3 +1,4 @@
-document.write("ostatnia modyfikacja strony".fontcolor("#FF0000").bold().fontsize(5)+"<br>"); 
+document.write("ostatnia modyfikacja strony".fontcolor("#FF0000").bold().fontsize(5)+"<br>"+"<strike>"); 
 
 document.write(document.lastModified);
+
